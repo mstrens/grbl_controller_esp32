225f99ec69af08eda72f41d9934f3baeba6cf1d3
mstrens <mstrens@yahoo.fr>
2021-04-11 15:38:04 +0200

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,8 @@
 #define _config_h
 
 
-#define ESP32_VERSION "vers.2.0.e"
+#define ESP32_VERSION "vers.2.0.f
+
 
 // decide if you will use Wifi or not (and how)
 #define WIFI ESP32_ACT_AS_STATION              // select between NO_WIFI, ESP32_ACT_AS_STATION, ESP32_ACT_AS_AP 
